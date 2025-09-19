60b251c143da6d2810024f4f7a71e0d1a42063ff
Kanyarat Benamad <159973709+GoodiesLavender@users.noreply.github.com>
2025-09-19 09:17:47 -0500

    update age asking function
    "update age asking function"

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-var background = document.getElementById('background-color');
-background.addEventListener('click', changeColor, false);
+var background = document.getElementById("background-color");
+background.addEventListener("click", changeColor, false);
 
-
-var colors = ["red", "aqua", "cornflowerblue", "salmon", "orange", "black", "purple", "pink"];
+var colors = [
+  "red",
+  "aqua",
+  "cornflowerblue",
+  "salmon",
+  "orange",
+  "black",
+  "purple",
+  "pink",
+];
 
 function changeColor() {
-   background.style.backgroundColor =  colors[Math.floor((Math.random()*8)+1)];
+  background.style.backgroundColor = colors[Math.floor(Math.random() * 8 + 1)];
+}
+
+var x = prompt("How old are you?", "answer");
+var correct = true;
+if (x <= 18) {
+  alert("Wow! you are junior");
+} else if (x <= 24) {
+  alert("You are getting to be an adult");
+} else {
+  alert("Wow, you are senior");
 }
